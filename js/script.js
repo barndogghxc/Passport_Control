90bfdaef90e05c2bcda37d0fc98a9c521c357101
barndogghxc <barndogghxc@yahoo.com>
2018-02-05 01:02:26 -0500

    questions

---
+++ js/script.js
@@ -0,0 +1,389 @@
+let questions = [
+
+"How many amendments does the Constitution have?"
+
+12
+
+27
+
+35
+
+42
+
+"We elect a U.S. Senator for how many years?"
+
+4
+
+8
+
+6
+
+10
+
+"We elect a President for how many years?"
+
+4
+
+12
+
+8
+
+5
+
+
+"If the President can no longer serve, who becomes President?"
+
+Chief of Staff
+
+The Vice President
+
+Secretary of State
+
+Lead General
+
+
+"What do we call the first ten amendments to the Constitution?"
+
+The Bill of Rights
+
+The Ten Commandments
+
+The Bill of Lading
+
+The List of Ten
+
+
+"Which is a right or freedom from the First Amendment?"
+
+Right to vote
+
+Right to avoid taxes
+
+Right to free exercise of religion
+
+Right to bear arms
+
+
+What was the purpose of the Declaration of Independence?
+
+Formed an alliance with South America
+
+Announced our independence from Great Britain
+
+Announced our independence from Germany
+
+Freed the slaves
+
+
+What is one purpose of the U.S. Constitution?
+
+Declared war
+
+Defines state laws
+
+Protects the basic rights of Americans
+
+Establishes treaties
+
+
+What severed ties with Great Britain?
+
+The President of the United States
+
+The Supreme Court
+
+The U.S. Constitution
+
+The Declaration of Independence
+
+
+Before he was President, Eisenhower was a general. What war was he in?
+
+World War II
+
+World War I
+
+Civil War
+
+Vietnam War
+
+
+Name one state that borders Canada
+
+California
+
+Idaho
+
+Ohio
+
+Massachusetts
+
+
+Name one state that borders Mexico
+
+Florida
+
+California
+
+Utah
+
+Ohio
+
+
+What is the capital of the United States?
+
+Washington
+
+Washington, D.C.
+
+New York City
+
+Chicago
+
+What is the name of the national anthem?
+
+O Say Can You See
+
+The Star-Spangled Banner
+
+America The Beautiful
+
+
+When do we celebrate Independence Day?
+
+December 25
+
+December 12
+
+July 4
+
+September 13
+
+
+How many stars does the flag have?
+
+50
+
+51
+
+12
+
+13
+
+Where is the Statue of Liberty?
+
+New York
+
+Florida
+
+Maine
+
+Boston
+
+
+Name the U.S. war between the North and the South
+
+Civil War
+
+The Cold War
+
+World War I
+
+World War II
+What happened at the Constitutional Convention?
+
+The Founding Fathers wrote the Constitution
+
+USA became independent
+
+Founding Fathers discovered the Red Coats might be approaching
+
+John Hancock Died
+Who was President during the Great Depression and World War II?
+
+Franklin Roosevelt
+
+Harry Truman
+
+Calvin Coolidge
+
+Richard Nixon
+Who was President during World War I?
+
+Woodrow Wilson
+
+George Washington
+
+Abe Lincoln
+
+John F. Kennedy
+What territory did the United States buy from France in 1803?
+
+New York
+
+Louisiana
+
+Mississippi
+
+California
+Watch Video To Learn More >
+Who is the “Father of Our Country”?
+
+Obama
+
+Washington
+
+Lincoln
+
+Jefferson
+When was the Constitution written?
+
+1902
+
+1787
+
+1802
+
+1998
+When was the Declaration of Independence adopted?
+
+July 4, 1962
+
+July 4, 1776
+
+July 4, 1600
+
+July 4, 1998
+Who wrote the Declaration of Independence?
+
+Lincoln
+
+Thomas Jefferson
+
+Bill Clinton
+
+George Washinton
+When must all men register for the Selective Service?
+
+16
+
+18
+
+21
+
+12
+When is the last day you can send in federal income tax forms?
+
+April 12
+
+April 16
+
+April 15
+
+October 15
+How old do citizens have to be to vote for President?
+
+16
+
+21
+
+18
+
+23
+Which of these states do not border Mexico?
+
+Oklahoma
+
+Texas
+
+Arizona
+
+California
+
+New Mexico
+What Ocean is on the East Coast of the United States?
+
+Pacific
+
+Atlantic
+
+Artic
+
+Indian
+How many U.S. Senators are there?
+
+89
+
+100
+
+134
+
+72
+We elect a U.S. Representative for how many years?
+
+6
+
+4
+
+2
+
+3
+In what month do we vote for President?
+
+April
+
+June
+
+November
+
+October
+Who signs bills to become laws?
+
+The Vice President
+
+The President
+
+The Senate
+
+The People
+What is the highest court in the United States?
+
+The District Courts
+
+The Supreme Court
+
+The Appellate Courts
+
+The County Court
+
+
+"How many justices are on the Supreme Court?"
+
+9
+
+13
+
+2
+
+5
+
+
+"What are the two major political parties in the United States?"
+
+Democratic & Republican
+
+Democratic & Independent
+
+Republican & Independent
+
+Independent & Liberal
+
+
+"What do we show loyalty to when we say the Pledge of Allegiance?"
+
+The Flag
+
+Our Parents
+
+The President
+];
