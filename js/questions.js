a51207672fed2254d6c6104c18e15bde7ea35d63
barndogghxc <barndogghxc@yahoo.com>
2018-02-07 14:24:19 -0500

    created external question bank

---
+++ js/questions.js
@@ -0,0 +1,357 @@
+let beginnerQuestions = [];
+let easyQuestions = [];
+let mediumQuestions = [];
+let hardQuestions = [];
+let deathEaterQuestions = [];
+
+ {
+ "question": "During the Cold War, what was the main concern of the United States?",
+ "choiceA": "Communism",
+ "choiceB": "Taliban",
+ "choiceC": "Mumble Rappers",
+ "choiceD": "Nepotism",
+ "answer": "A"}
+
+ {"question": "Which of the following is a U.S. territory?",
+ "choiceA":  "Philipines",
+ "choiceB":  "Thailand",
+ "choiceC": "Guam",
+ "choiceD":  "Hawaii",
+  "answer": "C" },
+
+ {"question": "Why does the flag have 13 stripes?",
+ "choiceA": "13 is a lucky number",
+ "choiceB":  "The original flagmaker had 13 children",
+ "choiceC":  "13 original colonies",
+ "choiceD":  "The 13 secret ingriedients to the chicken",
+  "answer": "C" },
+
+ {"question": "We elect a President for how many years?",
+ "choiceA": "4",
+ "choiceB":  "12",
+ "choiceC":  "8",
+ "choiceD":  "5",
+ "answer": "A"} ,
+
+ {"question": "If the President can no longer serve, who becomes President?",
+  "Chief of Staff",
+  "The Vice President",
+  "Secretary of State",
+  "Lead General",
+  "answer": "B"},
+
+ {"question": "What do we call the first ten amendments to the Constitution?",
+  "The Bill of Rights",
+  "The Ten Commandments",
+  "The Bill of Lading",
+  "The List of Ten",
+  "answer": "A"},
+
+ {"question": "Which is a right or freedom from the First Amendment?",
+  "Right to vote",
+  "Right to avoid taxes",
+  "Right to free exercise of religion",
+  "Right to bear arms",
+  "answer": "C" },
+
+ {"question": "What was the purpose of the Declaration of Independence?",
+ "Formed an alliance with South America",
+  "Announced our independence from Great Britain",
+  "Announced our independence from Germany",
+  "Freed the slaves",
+  "answer": "B"},
+
+ {"question": "What is one purpose of the U.S. Constitution?",
+
+ "Declared war",
+  "Defines state laws",
+  "Protects the basic rights of Americans",
+  "Establishes treaties",
+  "answer": "B"},
+
+ {"question": "What severed ties with Great Britain?",
+
+ "The President of the United States",
+  "The Supreme Court",
+  "The U.S. Constitution",
+  "The Declaration of Independence",
+  "answer": "D"},
+
+ {"question": "Before he was President,
+  Eisenhower was a general. What war was he in?",
+
+ "World War II",
+  "World War I",
+  "Civil War",
+  "Vietnam War",
+  "answer":
+  "A"},
+
+ {"question": "Name one state that borders Canada",
+
+ "New Jersey",
+  "Idaho",
+  "Ohio",
+  "Massachusetts",
+  "answer": "B"} ,
+
+ {"question": "Name one state that borders Mexico",
+
+ "Florida",
+  "California",
+  "Utah",
+  "Ohio",
+  "answer": "B"} ,
+
+ {"question": "What is the capital of the United States?",
+
+ "Washington",
+  "Washington,
+  D.C.",
+  "New York City",
+  "Chicago",
+  "answer": "B"} ,
+
+ {"question": "What is the name of the national anthem?",
+
+ "O Say Can You See",
+  "The Star-Spangled Banner",
+  "America The Beautiful",
+  "Whistle while you Twerk",
+  "answer": "B"},
+
+ {"question": "When do we celebrate Independence Day?",
+
+ "December 25",
+  "December 12",
+  "July 4",
+  "September 13",
+  "answer": "C" } ,
+
+ {"question": "How many stars does the flag have?",
+
+ "50",
+  "51",
+  "12",
+  "13",
+  "answer":
+  "A"} ,
+
+ "What did Susan B. Anthony do?",
+
+ "Fought for womens rights",
+  "Punched a Nazi",
+  "Went to General Assembly",
+  "Was an Instagram Model",
+  "answer":
+  "A"},
+
+ "Where is the Statue of Liberty?",
+
+ "New Jersey",
+  "Florida",
+  "Maine",
+  "Boston",
+  "answer":
+  "A"} ,
+
+ "Name the U.S. war between the North and the South",
+
+ "Civil War",
+  "The Cold War",
+  "World War I",
+  "World War II",
+  "answer":
+  "A"} ,
+
+ "What happened at the Constitutional Convention?",
+
+ "The Founding Fathers wrote the Constitution",
+  "USA became independent",
+  "Founding Fathers discovered the Red Coats might be approaching",
+  "John Hancock Died",
+  "answer":
+  "A"},
+
+ "Who was President during the Great Depression and World War II?",
+
+ "Franklin Roosevelt",
+  "Harry Truman",
+  "Calvin Coolidge",
+  "Richard Nixon",
+  "answer":
+  "A"},
+
+ "Who was President during World War I?",
+
+ "Woodrow Wilson",
+  "George Washington",
+  "Abe Lincoln",
+  "John F. Kennedy",
+  "answer":
+  "A"},
+
+ "What territory did the United States buy from France in 1803?",
+
+ "New York",
+  "Louisiana",
+  "Mississippi",
+  "California",
+  "answer": "B"},
+
+ "Who is the Father of Our Country?",
+
+ "Obama",
+  "Washington",
+  "Lincoln",
+  "Jefferson",
+  "answer": "B"},
+
+ "When was the Constitution written?",
+
+ "1902",
+  "1787",
+  "1802",
+  "1998",
+  "answer": "B"},
+
+ "When was the Declaration of Independence adopted?",
+
+ "July 4,
+  1962",
+  "July 4,
+  1776",
+  "July 4,
+  1600",
+  "July 4,
+  1998",
+  "answer": "B"},
+
+ "Who wrote the Declaration of Independence?",
+  "Lincoln",
+
+ "Thomas Jefferson",
+  "Bill Clinton",
+  "George Washinton",
+  "answer": "B"},
+
+ "When must all men register for the Selective Service?",
+
+ "16",
+  "18",
+  "21",
+  "12",
+  "answer": "B"},
+
+ "When is the last day you can send in federal income tax forms?",
+
+ "April 12",
+  "April 16",
+  "April 15",
+  "October 15",
+  "answer": "C" },
+
+ "How old do citizens have to be to vote for President?",
+
+ "16",
+  "21",
+  "18",
+  "23",
+  "answer": "C" },
+
+ "Which of these states do not border Mexico?",
+
+ "Texas",
+  "Arizona",
+  "California",
+  "New Jersey",
+  "answer": "D"},
+
+ "What Ocean is on the East Coast of the United States?",
+
+ "Pacific",
+  "Atlantic",
+  "Artic",
+  "Indian",
+  "answer": "B"},
+
+ "How many U.S. Senators are there?",
+
+ "89",
+  "100",
+  "134",
+  "72",
+  "answer": "B"},
+
+ "We elect a U.S. Representative for how many years?",
+  "6",
+  "4",
+  "2",
+  "3",
+  "answer": "C" },
+
+ "Why do some states have more Representatives than other states?",
+
+ "Because New Jersey is the best",
+  "Because screw Kansas",
+  "States Population",
+  "Snape killed Dumbledore",
+  "answer": "C" },
+
+ "Which of the following is not a U.S. National Holiday?",
+
+ "Martin Luther King Day",
+  "Veterans Day",
+  "Obama Day",
+  "Christmas",
+  "answer": "C" },
+
+ "In what month do we vote for President?",
+
+ "April",
+  "June",
+  "November",
+  "October",
+  "answer": "C" },
+
+ "question": "Who signs bills to become laws?",
+
+ "choice":"The Vice President",
+  "choice":"The President",
+  "choice":"The Senate",
+  "choice":"The People",
+  "answer": "B"},
+
+ "question": "What is the highest court in the United States?",
+
+ "choice":"The District Courts",
+  "choice":"The Supreme Court",
+  "choice":"The Appellate Courts",
+  "choice":"The County Court",
+  "answer": "B"},
+
+ "question": "How many justices are on the Supreme Court?",
+
+ "choice":"9",
+  "choice":"13",
+  "choice":"2",
+  "choice":"5",
+  "answer":
+  "A"},
+
+ "question": "What are the two major political parties in the United States?",
+
+ "choice":"Democratic & Republican",
+  "choice":"Democratic & Independent",
+  "choice":"Republican & Independent",
+  "choice":"Independent & Liberal",
+  "answer":
+  "A"},
+
+ "question": "What do we show loyalty to when we say the Pledge of Allegiance?",
+
+ "choiceA":"The Flag",
+  "choiceB":"The Army",
+  "choiceC":"The fictious man in the sky",
+  "choiceD":"The President",
+  "answer": "A"}
