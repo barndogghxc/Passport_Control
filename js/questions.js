7a42477758665a7b408ffcd8ae32eedbbeea938a
barndogghxc <barndogghxc@yahoo.com>
2018-02-07 14:54:44 -0500

    why did i make so many questions

--- js/questions.js
+++ js/questions.js
@@ -1,119 +1,127 @@
-let beginnerQuestions = [];
+let beginnerQuestions = [{
+ "question": : "During the Cold War, what was the main concern of the United States?",
+ "choiceA": "Communism",
+ "choiceB": "Taliban",
+ "choiceC": "Mumble Rappers",
+ "choiceD": "Nepotism",
+ "answer": "A"
+},
+
+ {
+  "question": "Which of the following is a U.S. territory?",
+  "choiceA":  "Philipines",
+  "choiceB":  "Thailand",
+  "choiceC": "Guam",
+  "choiceD":  "Hawaii",
+  "answer": "C" 
+},
+
+ {
+  "question": "Why does the flag have 13 stripes?",
+  "choiceA": "13 is a lucky number",
+  "choiceB":  "The original flagmaker had 13 children",
+  "choiceC":  "13 original colonies",
+  "choiceD":  "The 13 secret ingriedients to the chicken",
+  "answer": "C" 
+},
+
+ {
+  "question": "We elect a President for how many years?",
+  "choiceA": "4",
+  "choiceB":  "12",
+  "choiceC":  "8",
+  "choiceD":  "5",
+  "answer": "A"
+},
+
+ {
+  "question": "If the President can no longer serve, who becomes President?",
+  "choiceA": "Chief of Staff",
+  "choiceB": "The Vice President",
+  "choiceC": "Secretary of State",
+  "choiceD": "Lead General",
+  "answer": "B"
+},
+
+ {
+  "question": "What do we call the first ten amendments to the Constitution?",
+  "choiceA": "The Bill of Rights",
+  "choiceB": "The Ten Commandments",
+ "choiceC": "The Bill of Lading",
+  "choiceD": "The List of Ten",
+  "answer": "A"
+},
+
+ {
+  "question": "Which is a right or freedom from the First Amendment?",
+  "choiceA": "Right to vote",
+  "choiceB": "Right to avoid taxes",
+  "choiceC": "Right to free exercise of religion",
+  "choiceD": "Right to bear arms",
+  "answer": "C" 
+},
+
+ {
+  "question": "What was the purpose of the Declaration of Independence?",
+  "choiceA": "Formed an alliance with South America",
+  "choiceB":  "Announced our independence from Great Britain",
+  "choiceC":  "Announced our independence from Germany",
+  "choiceD":  "Freed the slaves",
+  "answer": "B"
+},
+
+ {
+  "question": "What is one purpose of the U.S. Constitution?",
+  "choiceA": "Declared war",
+  "choiceB":  "Defines state laws",
+  "choiceC":  "Protects the basic rights of Americans",
+  "choiceD":  "Establishes treaties",
+  "answer": "B"
+},
+
+ {
+  "question": "What severed ties with Great Britain?",
+  "choiceA": "The President of the United States",
+  "choiceB": "The Supreme Court",
+  "choiceC": "The U.S. Constitution",
+  "choiceD": "The Declaration of Independence",
+  "answer": "D"
+}];
 let easyQuestions = [];
 let mediumQuestions = [];
 let hardQuestions = [];
 let deathEaterQuestions = [];
 
+ 
+
  {
- "question": "During the Cold War, what was the main concern of the United States?",
- "choiceA": "Communism",
- "choiceB": "Taliban",
- "choiceC": "Mumble Rappers",
- "choiceD": "Nepotism",
- "answer": "A"}
+  "question": "Name one state that borders Canada.",
+  "choiceA": "New Jersey",
+  "choiceB": "Idaho",
+  "choiceC": "Ohio",
+  "choiceD": "Massachusetts",
+  "answer": "B"
+},
 
- {"question": "Which of the following is a U.S. territory?",
- "choiceA":  "Philipines",
- "choiceB":  "Thailand",
- "choiceC": "Guam",
- "choiceD":  "Hawaii",
-  "answer": "C" },
+ {
+  "question": "Name one state that borders Mexico.",
+  "choiceA": "Florida",
+  "choiceB": "California",
+  "choiceC": "Utah",
+  "choiceD": "Ohio",
+  "answer": "B"
+},
 
- {"question": "Why does the flag have 13 stripes?",
- "choiceA": "13 is a lucky number",
- "choiceB":  "The original flagmaker had 13 children",
- "choiceC":  "13 original colonies",
- "choiceD":  "The 13 secret ingriedients to the chicken",
-  "answer": "C" },
-
- {"question": "We elect a President for how many years?",
- "choiceA": "4",
- "choiceB":  "12",
- "choiceC":  "8",
- "choiceD":  "5",
- "answer": "A"} ,
-
- {"question": "If the President can no longer serve, who becomes President?",
-  "Chief of Staff",
-  "The Vice President",
-  "Secretary of State",
-  "Lead General",
-  "answer": "B"},
-
- {"question": "What do we call the first ten amendments to the Constitution?",
-  "The Bill of Rights",
-  "The Ten Commandments",
-  "The Bill of Lading",
-  "The List of Ten",
-  "answer": "A"},
-
- {"question": "Which is a right or freedom from the First Amendment?",
-  "Right to vote",
-  "Right to avoid taxes",
-  "Right to free exercise of religion",
-  "Right to bear arms",
-  "answer": "C" },
-
- {"question": "What was the purpose of the Declaration of Independence?",
- "Formed an alliance with South America",
-  "Announced our independence from Great Britain",
-  "Announced our independence from Germany",
-  "Freed the slaves",
-  "answer": "B"},
-
- {"question": "What is one purpose of the U.S. Constitution?",
-
- "Declared war",
-  "Defines state laws",
-  "Protects the basic rights of Americans",
-  "Establishes treaties",
-  "answer": "B"},
-
- {"question": "What severed ties with Great Britain?",
-
- "The President of the United States",
-  "The Supreme Court",
-  "The U.S. Constitution",
-  "The Declaration of Independence",
-  "answer": "D"},
-
- {"question": "Before he was President,
-  Eisenhower was a general. What war was he in?",
-
- "World War II",
-  "World War I",
-  "Civil War",
-  "Vietnam War",
-  "answer":
-  "A"},
-
- {"question": "Name one state that borders Canada",
-
- "New Jersey",
-  "Idaho",
-  "Ohio",
-  "Massachusetts",
-  "answer": "B"} ,
-
- {"question": "Name one state that borders Mexico",
-
- "Florida",
-  "California",
-  "Utah",
-  "Ohio",
-  "answer": "B"} ,
-
- {"question": "What is the capital of the United States?",
-
- "Washington",
-  "Washington,
-  D.C.",
-  "New York City",
-  "Chicago",
-  "answer": "B"} ,
+ {
+  "question": "What is the capital of the United States?",
+  "choiceA": "Washington",
+  "choiceB": "Washington, D.C.",
+  "choiceC": "New York City",
+  "choiceD": "Chicago",
+  "answer": "B"
+},
 
  {"question": "What is the name of the national anthem?",
-
  "O Say Can You See",
   "The Star-Spangled Banner",
   "America The Beautiful",
@@ -121,7 +129,6 @@ let deathEaterQuestions = [];
   "answer": "B"},
 
  {"question": "When do we celebrate Independence Day?",
-
  "December 25",
   "December 12",
   "July 4",
@@ -129,7 +136,6 @@ let deathEaterQuestions = [];
   "answer": "C" } ,
 
  {"question": "How many stars does the flag have?",
-
  "50",
   "51",
   "12",
@@ -138,7 +144,6 @@ let deathEaterQuestions = [];
   "A"} ,
 
  "What did Susan B. Anthony do?",
-
  "Fought for womens rights",
   "Punched a Nazi",
   "Went to General Assembly",
@@ -147,7 +152,6 @@ let deathEaterQuestions = [];
   "A"},
 
  "Where is the Statue of Liberty?",
-
  "New Jersey",
   "Florida",
   "Maine",
@@ -156,7 +160,6 @@ let deathEaterQuestions = [];
   "A"} ,
 
  "Name the U.S. war between the North and the South",
-
  "Civil War",
   "The Cold War",
   "World War I",
@@ -165,7 +168,6 @@ let deathEaterQuestions = [];
   "A"} ,
 
  "What happened at the Constitutional Convention?",
-
  "The Founding Fathers wrote the Constitution",
   "USA became independent",
   "Founding Fathers discovered the Red Coats might be approaching",
@@ -174,7 +176,6 @@ let deathEaterQuestions = [];
   "A"},
 
  "Who was President during the Great Depression and World War II?",
-
  "Franklin Roosevelt",
   "Harry Truman",
   "Calvin Coolidge",
@@ -183,7 +184,6 @@ let deathEaterQuestions = [];
   "A"},
 
  "Who was President during World War I?",
-
  "Woodrow Wilson",
   "George Washington",
   "Abe Lincoln",
@@ -192,7 +192,6 @@ let deathEaterQuestions = [];
   "A"},
 
  "What territory did the United States buy from France in 1803?",
-
  "New York",
   "Louisiana",
   "Mississippi",
@@ -200,7 +199,6 @@ let deathEaterQuestions = [];
   "answer": "B"},
 
  "Who is the Father of Our Country?",
-
  "Obama",
   "Washington",
   "Lincoln",
@@ -208,7 +206,6 @@ let deathEaterQuestions = [];
   "answer": "B"},
 
  "When was the Constitution written?",
-
  "1902",
   "1787",
   "1802",
@@ -216,7 +213,6 @@ let deathEaterQuestions = [];
   "answer": "B"},
 
  "When was the Declaration of Independence adopted?",
-
  "July 4,
   1962",
   "July 4,
@@ -229,14 +225,12 @@ let deathEaterQuestions = [];
 
  "Who wrote the Declaration of Independence?",
   "Lincoln",
-
  "Thomas Jefferson",
   "Bill Clinton",
   "George Washinton",
   "answer": "B"},
 
  "When must all men register for the Selective Service?",
-
  "16",
   "18",
   "21",
@@ -244,7 +238,6 @@ let deathEaterQuestions = [];
   "answer": "B"},
 
  "When is the last day you can send in federal income tax forms?",
-
  "April 12",
   "April 16",
   "April 15",
@@ -252,7 +245,6 @@ let deathEaterQuestions = [];
   "answer": "C" },
 
  "How old do citizens have to be to vote for President?",
-
  "16",
   "21",
   "18",
@@ -260,7 +252,6 @@ let deathEaterQuestions = [];
   "answer": "C" },
 
  "Which of these states do not border Mexico?",
-
  "Texas",
   "Arizona",
   "California",
@@ -268,90 +259,97 @@ let deathEaterQuestions = [];
   "answer": "D"},
 
  "What Ocean is on the East Coast of the United States?",
-
  "Pacific",
   "Atlantic",
   "Artic",
   "Indian",
   "answer": "B"},
 
- "How many U.S. Senators are there?",
-
- "89",
+{
+  "question":  "How many U.S. Senators are there?",
+ "choiceA": "89",
   "100",
   "134",
   "72",
   "answer": "B"},
 
- "We elect a U.S. Representative for how many years?",
-  "6",
-  "4",
+{
+  "question": 
+ "choiceA": "We elect a U.S. Representative for how many years?",
+  "choiceB": "6",
+  "choiceC":"4",
   "2",
-  "3",
+  "choiceD": "3",
   "answer": "C" },
 
- "Why do some states have more Representatives than other states?",
+ {
+  "question": "Why do some states have more Representatives than other states?",
+ "choiceA": "Because New Jersey is the best",
+  "choiceB": "Because screw Kansas",
+  "choiceC":"States Population",
+  "choiceD": "The Electoral College is a Conspiracy",
+  "answer": "C" 
+},
 
- "Because New Jersey is the best",
-  "Because screw Kansas",
-  "States Population",
-  "Snape killed Dumbledore",
-  "answer": "C" },
+ {
+  "question": "Which of the following is not a U.S. National Holiday?",
+ "choiceA": "Martin Luther King Day",
+  "choiceB": "Veterans Day",
+  "choiceC":"Obama Day",
+  "choiceD": "Christmas",
+  "answer": "C"
+},
 
- "Which of the following is not a U.S. National Holiday?",
+ {
+  "question": "In what month do we vote for President?",
+ "choiceA": "April",
+  "choiceB": "June",
+  "choiceC":"November",
+  "choiceD": "October",
+  "answer": "C"
+},
 
- "Martin Luther King Day",
-  "Veterans Day",
-  "Obama Day",
-  "Christmas",
-  "answer": "C" },
+ {
+  "question": "Who signs bills to become laws?",
+  "choiceA":"The Vice President",
+  "choiceB":"The President",
+  "choiceC":"The Senate",
+  "choiceD":"The People",
+  "answer": "B"
+},
 
- "In what month do we vote for President?",
+ {
+  "question": "What is the highest court in the United States?",
+  "choiceA":"The District Courts",
+  "choiceB":"The Supreme Court",
+  "choiceC":"The Appellate Courts",
+  "choiceD":"The County Court",
+  "answer": "B"
+},
 
- "April",
-  "June",
-  "November",
-  "October",
-  "answer": "C" },
+ {
+  "question": "How many justices are on the Supreme Court?",
+  "choiceA":"9",
+  "choiceB":"13",
+  "choiceC":"2",
+  "choiceD":"5",
+  "answer": "A"
+},
 
- "question": "Who signs bills to become laws?",
+ {
+  "question": "What are the two major political parties in the United States?",
+  "choiceA":"Democratic & Republican",
+  "choiceB":"Democratic & Independent",
+  "choiceC":"Republican & Independent",
+  "choiceD":"Independent & Liberal",
+  "answer": "A"
+},
 
- "choice":"The Vice President",
-  "choice":"The President",
-  "choice":"The Senate",
-  "choice":"The People",
-  "answer": "B"},
-
- "question": "What is the highest court in the United States?",
-
- "choice":"The District Courts",
-  "choice":"The Supreme Court",
-  "choice":"The Appellate Courts",
-  "choice":"The County Court",
-  "answer": "B"},
-
- "question": "How many justices are on the Supreme Court?",
-
- "choice":"9",
-  "choice":"13",
-  "choice":"2",
-  "choice":"5",
-  "answer":
-  "A"},
-
- "question": "What are the two major political parties in the United States?",
-
- "choice":"Democratic & Republican",
-  "choice":"Democratic & Independent",
-  "choice":"Republican & Independent",
-  "choice":"Independent & Liberal",
-  "answer":
-  "A"},
-
- "question": "What do we show loyalty to when we say the Pledge of Allegiance?",
-
- "choiceA":"The Flag",
-  "choiceB":"The Army",
-  "choiceC":"The fictious man in the sky",
-  "choiceD":"The President",
-  "answer": "A"}
+  {
+    "question": : "What do we show loyalty to when we say the Pledge of Allegiance?",
+    "choiceA":"The Flag",
+    "choiceB":"The Army",
+    "choiceC":"The fictious man in the sky",
+    "choiceD":"The President",
+    "answer": "A"
+}
